420d1d8560a7ef95ec255ccec28a0ef46be7f623
fxlr8 <gt@csgo.com>
2021-09-22 23:23:23 +0300

    render allow list

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,16 +1,24 @@
 import React, { FC, useEffect } from 'react'
 import browser from 'webextension-polyfill'
 import useAppState from './useAppState'
+import useAllowList from './useAllowList'
 import Switch from './Switch'
 
 const App: FC = () => {
+    const [allowList, allowDomain, unallowDomain] = useAllowList()
+
     const { domain } = useAppState()
+
+    const enabled = !allowList.includes(domain!)
 
     return (
         <div>
             <div>{domain}</div>
             Hello world!
-            <Switch active={true} onClick={() => console.log('click')} />
+            <Switch active={enabled} onClick={enabled ? () => allowDomain(domain) : () => unallowDomain(domain)} />
+            <div>
+                {allowList.map((domain) => (<div key={domain}>{domain}</div>))}
+            </div>
         </div>
     )
 }
