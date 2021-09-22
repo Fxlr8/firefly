0620d1a6606976af0fd65455e13e61c47200bdbd
fxlr8 <gt@csgo.com>
2021-09-22 18:24:09 +0300

    logging all requests

--- src/background.ts
+++ src/background.ts
@@ -1,1 +1,21 @@
 import 'webextension-polyfill'
+import { parse } from 'tldts'
+
+console.log('HELLO BACKGROUND')
+
+
+const logURL = (requestDetails: browser.webRequest._OnBeforeRequestDetails): void => {
+    const requestData = {
+        url: requestDetails.url,
+        third: requestDetails.thirdParty,
+        tld: parse(requestDetails.url)
+    }
+    console.log(requestData)
+}
+
+browser.webRequest.onBeforeRequest.addListener(
+    logURL,
+    { urls: ["<all_urls>"] }
+);
+
+
