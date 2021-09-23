52f9ffab152730f8eee0ff4efae0ddb0cd7471b9
fxlr8 <gt@csgo.com>
2021-09-23 12:54:06 +0300

    reload tab on allow list change

--- src/background.ts
+++ src/background.ts
@@ -19,21 +19,34 @@ const sendState = async () => {
 
 }
 
+const reloadTab = async () => {
+    const tabs = await browser.tabs.query({ active: true, currentWindow: true })
+    if (tabs.length) {
+        browser.tabs.reload(tabs[0].id)
+    }
+}
+
 browser.runtime.onMessage.addListener((message: Action, sender) => {
     // only listen to own messages
     if (sender.id !== browser.runtime.id) return
 
     if (!message) return
 
-    console.log(sender?.tab?.url)
+    console.log(message, sender)
 
     switch (message.type) {
         case 'sync': {
             return sendState()
         }
+        case 'allowListChange': {
+            console.log('Allow list changed')
+            return reloadTab()
+        }
         default: console.warn('Unknown action', message)
 
     }
+    console.log(tabs)
+})
 
     console.log(message, sender)
 })
