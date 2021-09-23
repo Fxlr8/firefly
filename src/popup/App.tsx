7a97ee492b495e0f90a0260fb1db5fb448af932b
fxlr8 <gt@csgo.com>
2021-09-23 16:59:47 +0300

    update app to use new hooks

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,23 +1,26 @@
 import React, { FC, useEffect } from 'react'
-import browser from 'webextension-polyfill'
-import useAppState from './useAppState'
+import useTrackerCount from './useTrackerCount'
 import useAllowList from './useAllowList'
+import useTabInfo from './useTabInfo'
 import Switch from './Switch'
 
 const App: FC = () => {
     const [allowList, allowDomain, unallowDomain] = useAllowList()
 
-    const { domain } = useAppState()
+    const [hostname, tabId] = useTabInfo()
 
-    const enabled = !allowList.includes(domain!)
+    const trackerCount = useTrackerCount(tabId)
+
+    const enabled = !allowList.includes(hostname!)
 
     return (
         <div>
-            <div>{domain}</div>
+            <div>{hostname}</div>
+            <div>{trackerCount}</div>
             Hello world!
-            <Switch active={enabled} onClick={enabled ? () => allowDomain(domain) : () => unallowDomain(domain)} />
+            <Switch active={enabled} onClick={enabled ? () => allowDomain(hostname) : () => unallowDomain(hostname)} />
             <div>
-                {allowList.map((domain) => (<div key={domain}>{domain}</div>))}
+                {allowList.map((hostname) => (<div key={hostname}>{hostname}</div>))}
             </div>
         </div>
     )
